cba353cf54820d08c64d6d718e2bdb0053861dea
b03902017 <b03902017@ntu.edu.tw>
2016-01-16 22:54:04 +0800

    add four messages

---
+++ src/liardice/BidMessage.java
@@ -0,0 +1,15 @@
+package liardice;
+
+import java.io.Serializable;
+
+public class BidMessage implements Serializable {
+    public final String id;
+	public final int numberOfDice;
+	public final int valueOfDice;
+    
+	public BidMessage(String id, int numberOfDice, int valueOfDice) {
+        this.id = id;
+        this.numberOfDice = numberOfDice;
+		this.valueOfDice = valueOfDice;
+    }
+}
