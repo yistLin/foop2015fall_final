192427fc0cb6faff4b1d1e6adec2d92cbf16c036
JCly-rikiu <jk123456789jp@gmail.com>
2016-01-17 18:37:41 +0800

    Remove merge charachter

--- src/liardice/BidMessage.java
+++ src/liardice/BidMessage.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class BidMessage implements Serializable {
 	public final int numberOfDice;
 	public final int valueOfDice;
-    
+
 	public BidMessage(int numberOfDice, int valueOfDice) {
         this.numberOfDice = numberOfDice;
         this.valueOfDice = valueOfDice;
