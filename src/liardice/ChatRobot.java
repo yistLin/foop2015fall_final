bc63eee5898431018b2bb2d305c040ce37efd7b8
Yist Lin <yishen992@gmail.com>
2016-01-18 20:48:47 +0800

    add talk() in ChatRobot

--- src/liardice/ChatRobot.java
+++ src/liardice/ChatRobot.java
@@ -8,7 +8,14 @@ import java.util.Date;
 
 public class ChatRobot {
 
-    public static final int demo = 0;
+    public static final int GET_DICE = 0;
+    public static final int ROUND_START = 1;
+    public static final int DO_BID = 2;
+    public static final int DO_CATCH = 3;
+    public static final int NO_CATCH = 4;
+    public static final int YES_CATCH = 5;
+    public static final int ROUND_END = 6;
+    public static final int OTHER_TALK = 7;
 
     public final String NICKNAME;
 
@@ -27,5 +34,25 @@ public class ChatRobot {
     	Random rnd = new Random(seed);
     	int nameIndex = rnd.nextInt(filenames.length);
     	NICKNAME = filenames[nameIndex];
+    }
+
+    // GET_DICE
+    public String talk(int status, Dice[] dice) {
+    	return null;
+    }
+
+    // ROUND_START, DO_BID, DO_CATCH, NO_CATCH, YES_CATCH
+    public String talk(int status) {
+    	return null;
+    }
+
+    // ROUND_END
+    public String talk(int status, boolean imLoser) {
+    	return null;
+    }
+
+    // OTHER_TALK
+    public String talk(int status, String chat) {
+    	return null;
     }
 }
